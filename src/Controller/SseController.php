4973094a8d47ef6d561977204f4d2ccf7e8d3fa6
Simon Chabrier <simonchabrier@gmail.com>
2023-04-26 16:23:59 +0200

    claen comments

--- src/Controller/SseController.php
+++ src/Controller/SseController.php
@@ -11,13 +11,19 @@ use Symfony\Component\Mercure\Update;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SseController extends AbstractController
-{
+{   
+
     /**
      * @Route("/publish", name="app_publish")
      */
-    public function index(HubInterface $hub, Request $request): Response
+    public function index(
+            HubInterface $hub, 
+            Request $request
+            ): Response
     {   
-        $data = json_decode($request->getContent(), true);
+        $data = json_decode(
+            $request->getContent(), true // getContent() récupère le body de la requête
+        );
 
         $update = new Update(
             'montopic',
