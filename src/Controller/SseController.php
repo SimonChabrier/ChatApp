a13776e735dffcd593982056c90a690e7cdc7622
Simon Chabrier <simonchabrier@gmail.com>
2023-04-26 17:05:41 +0200

    dispatch topic notifiaction to mercure using sync messenger handler

--- src/Controller/SseController.php
+++ src/Controller/SseController.php
@@ -2,35 +2,38 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Message\Mercure;
+use Symfony\Component\Mercure\Update;
+use Symfony\Component\Mercure\HubInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mercure\HubInterface;
-use Symfony\Component\Mercure\Update;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SseController extends AbstractController
 {   
 
     /**
+     * Route qui récupère les données du formulaire les dipatche dans le bus
+     * Le bus les envoie ensuite au handler (MercureHandler) qui les publie dans le hub.
+     * 
      * @Route("/publish", name="app_publish")
      */
     public function index(
-            HubInterface $hub, 
+            MessageBusInterface $bus,
             Request $request
             ): Response
     {   
         $data = json_decode(
             $request->getContent(), true // getContent() récupère le body de la requête
         );
-
-        $update = new Update(
-            'montopic',
-            $data
-        );
-
-        $hub->publish($update);
+        
+        // Ici on dispatche en synchrone en interne car le hub Meruure est asynchrone par défaut de son côté
+        // donc c'est lui qui va gérer l'envoi des données aux clients sans bloquer le serveur de cette app.
+        // et qu'on a besoin de récupérer la réponse pour l'afficher dans la vue tout de suite.
+        $bus->dispatch(new Mercure('montopic', $data));
 
         return new JsonResponse([
             'retour' => $data
